343633675b21e6d3a2a37e1e9e419a8850311d1a
pateldhruvi08 <dhruvi080504@gmail.com>
2026-04-01 20:01:11 +0530

    initial commit

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -16,10 +16,8 @@ def get_password_hash(password):
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    # Permanent Authentication (100 Years)
+    expire = datetime.utcnow() + timedelta(days=36500)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
